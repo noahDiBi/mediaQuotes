339f6d8770967e264f25be861e38ef7686c5f5ed
Noah DiBiasi <noah.dibiasi@concurrenteda.com>
2021-12-05 21:51:21 -0500

    Organized output activity and recycler view

--- MediaQuotes/app/src/main/java/com/example/mediaquotes/OutputActivity.java
+++ MediaQuotes/app/src/main/java/com/example/mediaquotes/OutputActivity.java
@@ -1,14 +1,38 @@
 package com.example.mediaquotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.LinkedList;
+
 public class OutputActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private QuotesAdapter mAdapter;
+    private LinkedList<String> mQuoteList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
+
+        //Gets Linked List of quotes for recycler view
+        //Thanks stackOverflow
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            mQuoteList = (LinkedList<String>) extras.get("key");
+        }
+
+        //Create the data for the recyclerView
+        //instantiate recyclerView
+        mRecyclerView = findViewById(R.id.quoteList);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //create the adapter with the context of the MainActiviy and
+        //the data source
+        mAdapter = new QuotesAdapter(this, mQuoteList);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ MediaQuotes/app/src/main/java/com/example/mediaquotes/QuotesAdapter.java
@@ -0,0 +1,60 @@
+package com.example.mediaquotes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.LinkedList;
+
+public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.QuotesHolder>{
+
+    private LayoutInflater mInflater;
+    private LinkedList<String> mQuotes;
+    private Context context;
+
+    public QuotesAdapter(Context context,
+                           LinkedList<String> quoteList) {
+        //use this to create the layout
+        mInflater = LayoutInflater.from(context);
+        mQuotes = quoteList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public QuotesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.quote_item, parent, false);
+        return new QuotesHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuotesHolder holder, int position) {
+        String mCurrent = mQuotes.get(position);
+        holder.mQuoteView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mQuotes.size();
+    }
+
+    class QuotesHolder extends RecyclerView.ViewHolder{
+
+        private TextView mQuoteView;
+        private QuotesAdapter mAdapter;
+
+        public QuotesHolder(@NonNull View itemView, QuotesAdapter adapter) {
+            super(itemView);
+            mQuoteView = itemView.findViewById(R.id.quoteView);
+            mAdapter = adapter;
+        }
+    }
+
+}
