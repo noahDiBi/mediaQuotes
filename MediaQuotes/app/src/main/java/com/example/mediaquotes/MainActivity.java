34e460ace3bfe41d603832b2ad4017722bce3e32
BilgeDenizKocakk <85181818+BilgeDenizKocakk@users.noreply.github.com>
2021-12-07 09:23:12 -0500

    displays a message about which button is clicked. the structure could be helpful later

--- MediaQuotes/app/src/main/java/com/example/mediaquotes/MainActivity.java
+++ MediaQuotes/app/src/main/java/com/example/mediaquotes/MainActivity.java
@@ -6,31 +6,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private LinkedList<String> mQuotesText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity","Test");
         mQuotesText = new LinkedList<String>();
+
+        Button dune = findViewById(R.id.dune);
+        Button bb = findViewById(R.id.breakingbad);
+        Button got = findViewById(R.id.got);
+        Button bcs = findViewById(R.id.bettercallsaul);
+        Button lucifer = findViewById(R.id.lucifer);
+
+        dune.setOnClickListener(this);
+        bb.setOnClickListener(this);
+        got.setOnClickListener(this);
+        bcs.setOnClickListener(this);
+        lucifer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick (View v) {
+        switch(v.getId()) {
+            case R.id.dune:
+                Toast.makeText(this, "Dune is Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.breakingbad:
+                Toast.makeText(this, "Breaking is Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.got:
+                Toast.makeText(this, "Game of Thrones is Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bettercallsaul:
+                Toast.makeText(this, "Better Call Saul is Selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.lucifer:
+                Toast.makeText(this, "Lucifer is Selected", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     public void processInput(View view){
-
         Intent intent = new Intent(this, OutputActivity.class);
         FetchQuotes fetch = new FetchQuotes(mQuotesText);
         fetch.execute();
-        Log.d("MainActivity", "bruh");
         intent.putExtra("key", mQuotesText);
         startActivity(intent);
-
     }
-
 }
